23478943239a358612b7504ced48469ea1bce95b
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2023-08-18 08:42:29 +0500

    Write BoxScreen boilerplate

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const BoxScreen = () => {
+    return (
+        <View style={styles.viewStyle}>
+            <Text style={styles.textStyle}>BoxScreen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 1,
+        borderColor: 'black'
+    },
+    textStyle: {
+        borderWidth: 1,
+        borderColor: 'red'
+    }
+});
+
+export default BoxScreen;
