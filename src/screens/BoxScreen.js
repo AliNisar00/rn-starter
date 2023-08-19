5c728962532372b7c8732d6258a30cbeb2d3d89d
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2023-08-19 10:10:19 +0500

    Add BoxObjectModel, FlexBox, Position layoutstyles

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -2,21 +2,63 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
-    return (
+    /*return (
         <View style={styles.viewStyle}>
-            <Text style={styles.textStyle}>BoxScreen</Text>
+            <Text style={styles.textOneStyle}>Child #1</Text>
+            <Text style={styles.textTwoStyle}>Child #2</Text>
+            <Text style={styles.textThreeStyle}>Child #3</Text>
         </View>
     );
+    */
+   return (
+        <View style={styles.viewParentStyle}>
+            <View style={styles.viewOneStyle} />
+            <View style={styles.viewTwoStyle} />
+            <View style={styles.viewThreeStyle} />
+        </View>
+   );
 };
 
 const styles = StyleSheet.create({
     viewStyle: {
-        borderWidth: 1,
-        borderColor: 'black'
+        borderWidth: 3,
+        borderColor: 'black',
+        alignItems: 'flex-start', // part of Flex Box
+        //height: 200,
+        //flexDirection: 'row', // part of Flex Box
+        //justifyContent: 'center' // part of Flex Box
     },
-    textStyle: {
-        borderWidth: 1,
+    textOneStyle: {
+        borderWidth: 3,
         borderColor: 'red'
+    },
+    textTwoStyle: {
+        borderWidth: 3,
+        borderColor: 'red'
+    },
+    textThreeStyle: {
+        borderWidth: 3,
+        borderColor: 'red'
+    },
+    viewParentStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    viewOneStyle: {
+        height: 100,
+        width: 100,
+        backgroundColor: 'red'
+    },
+    viewTwoStyle: {
+        height: 100,
+        width: 100,
+        backgroundColor: 'green',
+        top: 100
+    },
+    viewThreeStyle: {
+        height: 100,
+        width: 100,
+        backgroundColor: 'purple'
     }
 });
 
